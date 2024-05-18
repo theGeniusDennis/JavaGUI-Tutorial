77df87dbd23994960b0c5b09d5676af7311db63e
theGeniusDennis <danim005@st.ug.edu.gh>
2024-05-18 13:09:11 -0700

    I changed the logo of the JFrame, and I also explored ways of changing the backgroundcolor of the JFrame.

--- Frame.java
+++ Frame.java
@@ -1,3 +1,4 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Frame {
@@ -5,7 +6,12 @@ public class Frame {
         //JFrame = a GUI  window to add compnents to
         JFrame frame =  new JFrame(); // creates a frame
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out of application
+        frame.setTitle("A simple Jframe window"); //adds title to the frame
+        frame.setResizable(false); //sets whether frame should be resizable
         frame.setSize(450, 450); //sets the x-dimension and the y-dimension
         frame.setVisible(true); // makes frame visible
+
+       
     }
 }
