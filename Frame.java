3b7ce0a496f8c5ebd6f7b6f5b4bcbab8f4862287
theGeniusDennis <danim005@st.ug.edu.gh>
2024-05-18 13:02:30 -0700

    Explored various ways of manipulating the JFrame

--- Frame.java
+++ Frame.java
@@ -1,5 +1,11 @@
+import javax.swing.JFrame;
+
 public class Frame {
     public static void main(String[] args) {
-        System.out.println("Heyy");
+        //JFrame = a GUI  window to add compnents to
+        JFrame frame =  new JFrame(); // creates a frame
+
+        frame.setSize(450, 450); //sets the x-dimension and the y-dimension
+        frame.setVisible(true); // makes frame visible
     }
 }
